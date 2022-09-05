e35e9e8fc5eb69a9ca448e2573b051a8a77104b7
Cuong Nguyen <ancuongnguyen07@gmail.com>
2022-09-05 17:18:11 +0300

    [lab2][ex3] sha256 python implementation

---
+++ lab2/python/Hash/sha256.py
@@ -0,0 +1,24 @@
+from Crypto.Hash import SHA256
+import sys
+
+# insprired by https://pycryptodome.readthedocs.io/en/latest/src/hash/sha256.html
+
+def compute_hash(file):
+    hash_object = SHA256.new(open(file, 'rb').read())
+    return hash_object.hexdigest()
+
+def verify_hash(file, given_hash):
+    hash = compute_hash(file)
+    return hash == given_hash
+
+def main():
+    mode = sys.argv[1]
+    if mode == '-c':
+        file_path = sys.argv[2]
+        print(compute_hash(file_path))
+    elif mode == '-v':
+        file_path, hash = sys.argv[2:4]
+        print(hash == compute_hash(file_path))
+
+if __name__ == '__main__':
+    main()
